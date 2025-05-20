26e9a57837cbd6733a881f6b509a6cf6b15ca310
abulfeedy <66095707+abulfeedy@users.noreply.github.com>
2025-05-20 01:10:45 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,39 +10,44 @@ const FooterSection = lazy(() => import("@/components/Footer"));
 import Cube from "/cube.svg"
 
 const Home = ({ onSectionRefs }) => {
-  const sectionRefs = {
-    home: useRef(null),
-    services: useRef(null),
-    projects: useRef(null),
-    about: useRef(null),
-    testimonials: useRef(null),
-  };
+  const heroRef = useRef(null);
+  const aboutRef = useRef(null);
+  const servicesRef = useRef(null);
+  const projectsRef = useRef(null);
+  const testimonialsRef = useRef(null);
 
   useEffect(() => {
+    const sectionRefs = {
+      home: heroRef,
+      about: aboutRef,
+      services: servicesRef,
+      projects: projectsRef,
+      testimonials: testimonialsRef,
+    };
     onSectionRefs(sectionRefs);
   }, [onSectionRefs]);
 
   return (
     <main>
       <Suspense fallback={
-  <div className="fixed inset-0 flex items-center justify-center bg-slate-800">
-      <img src={Cube} alt="Loading..." className="w-12 h-12 animate-spin" />
-    </div>
-  }
-> 
-        <div ref={sectionRefs.home } data-section='home'>
+        <div className="fixed inset-0 flex items-center justify-center bg-slate-800">
+            <img src={Cube} alt="Loading..." className="w-12 h-12 animate-spin" />
+          </div>
+        }
+      > 
+        <div ref={heroRef} data-section='home'>
           <HeroSection />
         </div>
-        <div ref={sectionRefs.services} data-section='services'>
+        <div ref={servicesRef} data-section='services'>
           <WhatWeDoSection />
         </div>
-        <div ref={sectionRefs.projects} data-section='projects'>
+        <div ref={projectsRef} data-section='projects'>
           <OurWorkSection />
         </div>
-        <div ref={sectionRefs.about} data-section='about'>
+        <div ref={aboutRef} data-section='about'>
           <AboutUsSection />
         </div>
-        <div ref={sectionRefs.testimonials} data-section='testimonials'>
+        <div ref={testimonialsRef} data-section='testimonials'>
           <TestimonialSection />
         </div>
         <FooterSection />
